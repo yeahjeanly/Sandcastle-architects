23ba1e0fd6ead57e54363033658edbf4265ab1bc
admin <admin@adminui-MacBookPro.local>
2025-11-11 20:44:34 +0900

    1111

---
+++ level01/level01.py
@@ -0,0 +1,17 @@
+ode = input()
+last_four_words = code[-4:]
+
+if last_four_words == "_eye":
+    print("Ophthalmologyc")
+
+elif last_four_words=="head":
+    print("Neurosurgery")
+
+elif last_four_words=="infl":
+    print("Orthopedics")
+
+elif last_four_words=="skin":
+    print("Dermatology")
+
+else:
+    print("direct recommendation")
