0ea2670343b9c25b6c4754867a00a9ae6096a525
yeahjeanly <jeongyeye88@gmail.com>
2025-11-11 20:41:52 +0900

    1111

---
+++ lv0-4.py
@@ -0,0 +1,9 @@
+code = input()
+last_four_words = code[-4:]
+
+if last_four_words == "_eye":
+    print("Ophthalmologyc")
+elif last_four_words == "head":
+    print("Neurosurgery")
+elif last_four_words == "infl":
+    print("Orthopedics")
