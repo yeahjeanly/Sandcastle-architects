f3f018fd93b02e0a82e19757fdc56ef9cd392800
gina6545 <gina6545@gmail.com>
2026-01-08 20:52:21 +0900

    0108 python

---
+++ 0108.py
@@ -0,0 +1,13 @@
+# 라면과 김밥 가격
+ramen = 1000
+kimbap = 800
+
+# 구매 개수
+ramen_count = 2
+kimbap_count = 3
+
+# 총 가격 계산
+total_price = ramen * ramen_count + kimbap * kimbap_count
+
+# 결과 출력
+print(total_price)
