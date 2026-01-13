c924427b394eee278e5c532a64c4d80e6f3707d3
gina6545 <gina6545@gmail.com>
2026-01-13 20:47:50 +0900

    0113 스터디

---
+++ JN0113.py
@@ -0,0 +1,10 @@
+# 변수를 이용해 코드 작성해보기
+name = '해리포터'
+age = 15
+country = 'UK'
+print(name, '나이', age, '국적', country)
+
+# 변수 name, age 를 이용해 코드 출력
+name = "해리포터"
+age = 15
+print('이름', name, '나이', 15)
